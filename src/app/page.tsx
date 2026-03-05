ae6482e3742ea5c20bfefdf58b38129917a1f339
Jorge Calderon <jcalderon@itsolutions-dj.com>
2026-03-05 15:21:35 -0500

    Redirigir raíz a /en

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
+import { redirect } from 'next/navigation'
+
+export default function RootPage() {
+  redirect('/en')
+}
+
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
